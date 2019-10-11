a1f9ff30b4705322bcd9969e3500793d1b6844eb
gitMission <missionrenjr@gmail.com>
2019-10-11 16:02:41 +0800

    making it better

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('it works');
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => { console.log(`Server start on port ${port}`) });
