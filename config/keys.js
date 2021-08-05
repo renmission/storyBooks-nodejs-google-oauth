3d0d060801a14563df0fad3079d43d9c68bc5ce6
Ren Mission <missionrenjr@gmail.com>
2021-08-05 16:36:22 +0800

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://storybooks-dev:storybooks-dev@cluster0-l7gi6.mongodb.net/test?retryWrites=true&w=majority',
-    googleClientID: '300369663985-2srg88fo8dknm9a2eq344a9uh4l5a21t.apps.googleusercontent.com',
-    googleClientSecret: '8S9cu10_we92WALSNHP38cIT'
-}
